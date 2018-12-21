0cca50585596c572af28487cc112748fbf1309f2
Diana Badas <diana.badas@gmail.com>
2018-12-21 14:43:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word (sentence)
   arr = []
   arr_reversed = []
-  arr << sentence.split(" ")
+  arr << sentence.split(/\W+/)
   arr.each do |word|
     arr_reversed << word.reverse
   end
