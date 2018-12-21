33616a3bd4874d3fa2194dc370200f7f4871c50e
Diana Badas <diana.badas@gmail.com>
2018-12-21 14:57:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,17 @@
-
+def reverse_each_word (sentence)
+  arr_with_sentence = sentence.split(" ")
+  array_reversed_words = []
+  arr_with_sentence.each do |word|
+    array_reversed_words << word.reverse
+  end
+  array_reversed_words.join(" ")
+end
 
 def reverse_each_word (sentence)
   arr_with_sentence = sentence.split(" ")
+  array_result = []
   arr_with_sentence.collect do |word|
-    word.reverse
+    array_result << word.reverse
   end
-  array_reversed_words.join(" ")
+  arr_with_sentence.join(" ")
 end
